2cf977f2260a4aa436a140bcecd4f03495dd4df5
Kate Mitchell <mail@katemitchell.org.uk>
2018-04-30 22:34:04 +0100

    Fix directory structure. Get secret key and debug settings from env. Update .gitignore file.

--- baggysoap/settings.py
+++ baggysoap/settings.py
@@ -22,10 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2b!m8okdml+n$=y@hi%-%%g!ph&j81rwoz92cm@)cv3k$i1!0d'
+# SECRET_KEY = '2b!m8okdml+n$=y@hi%-%%g!ph&j81rwoz92cm@)cv3k$i1!0d'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
