70e673b1ef50a249bf719449f48a08aca0ae70fd
Kate Mitchell <mail@katemitchell.org.uk>
2018-05-01 22:14:38 +0100

    Fix local DB issue by reading local .env in settings.py. Added psycopg2 to requirements.

--- baggysoap/settings.py
+++ baggysoap/settings.py
@@ -11,10 +11,43 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import re
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import dj_database_url
 
+
+def read_env():
+    """
+    Pulled from Honcho code with minor updates, reads local default
+    environment variables from a .env file in the root directory
+    """
+    try:
+        with open('.env') as f:
+            content = f.read()
+    except IOError:
+        content = ''
+
+    for line in content.splitlines():
+        m1 = re.match(r'\A([A-Za-z_0-9]+)=(.*)\Z', line)
+        if m1:
+            key, val = m1.group(1), m1.group(2)
+            m2 = re.match(r"\A'(.*)'\Z", val)
+            if m2:
+                val = m2.group(1)
+            m3 = re.match(r'\A"(.*)"\Z', val)
+            if m3:
+                val = re.sub(r'\\(.)', r'\1', m3.group(1))
+            os.environ.setdefault(key, val)
+
+
+def bool_env(name, default=False):
+    value = os.environ.get(name, default)
+    return False if value in ['False', 'false'] else bool(value)
+
+
+read_env()
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
