1ea470b5ab0249b6535953230b352abec586b827
Kate Mitchell <mail@katemitchell.org.uk>
2018-06-05 15:33:38 +0100

    Create SoapLoaf and SoapBar models and add admin views for all models

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,11 +1,37 @@
 from django.contrib import admin
-from .models import Bag, Soap, BaggySoap
-
-
-admin.site.register(Soap)
-admin.site.register(BaggySoap)
+from .models import Bag, SoapLoaf, BaggySoap, SoapBar
 
 
 @admin.register(Bag)
 class BagAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'bag_colour', 'label_colour', 'drawstring_colour', 'units', 'cost_price')
+
+
+@admin.register(SoapLoaf)
+class SoapLoafAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'fragrance', 'colour', 'units', 'cost_price')
+
+
+@admin.register(SoapBar)
+class SoapBarAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'fragrance', 'colour', 'units', 'cost_price', 'sell_price')
+
+    def name(self, obj):
+        return obj.loaf.name
+
+    def fragrance(self, obj):
+        return obj.loaf.fragrance
+
+    def colour(self, obj):
+        return obj.loaf.colour
+
+
+@admin.register(BaggySoap)
+class BaggySoapAdmin(admin.ModelAdmin):
+    list_display = ('id', 'bag_name', 'soap_name', 'units', 'cost_price', 'sell_price')
+
+    def bag_name(self, obj):
+        return obj.bag.name
+
+    def soap_name(self, obj):
+        return obj.soap.loaf.name
--- catalogue/models.py
+++ catalogue/models.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+from django.contrib.postgres.fields import JSONField
 from django.db import models
 
 COLOURS = (
@@ -22,34 +24,63 @@ class Bag(models.Model):
     #supplier
 
     units = models.PositiveIntegerField()
-    cost_price = models.DecimalField(decimal_places=2, max_digits=4)
+    cost_price = models.DecimalField(decimal_places=2, max_digits=4, help_text="Cost per bag, including shipping")
     sell_price = models.DecimalField(decimal_places=2, max_digits=4)
 
     def __str__(self):
         return self.name
 
 
-class Soap(models.Model):
+class SoapLoaf(models.Model):
     name = models.CharField(max_length=64)
     fragrance = models.CharField(max_length=64)
     colour = models.CharField(max_length=64)
 
+    ingredients = JSONField(default=list, blank=True, null=True)
+    weight = models.PositiveIntegerField(verbose_name="Weight (in kg)")
+
     #supplier
 
     units = models.PositiveIntegerField()
-    cost_price = models.DecimalField(decimal_places=2, max_digits=4)
+    bars_per_loaf = models.PositiveIntegerField()
+
+    cost_price = models.DecimalField(decimal_places=2, max_digits=4, help_text="Cost per loaf, including shipping")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Soap Loaf'
+
+
+class SoapBar(models.Model):
+    loaf = models.ForeignKey(SoapLoaf, on_delete=models.CASCADE)
+
+    units = models.PositiveIntegerField()
+
     sell_price = models.DecimalField(decimal_places=2, max_digits=4)
+
+    @property
+    def cost_price(self):
+        return round(Decimal(self.loaf.cost_price / self.loaf.bars_per_loaf), 2)
+
+    def __str__(self):
+        return self.loaf.name
+
+    class Meta:
+        verbose_name = 'Soap Bar'
 
 
 class BaggySoap(models.Model):
     bag = models.ForeignKey(Bag, on_delete=models.CASCADE)
-    soap = models.ForeignKey(Soap, on_delete=models.CASCADE)
+    soap = models.ForeignKey(SoapBar, on_delete=models.CASCADE)
 
     units = models.PositiveIntegerField()
     sell_price = models.DecimalField(decimal_places=2, max_digits=4)
 
+    @property
     def cost_price(self):
-        return self.bag.cost_price + self.soap.cost_price
+        return round(Decimal(self.bag.cost_price + self.soap.cost_price), 2)
 
     class Meta:
         verbose_name = 'Baggy Soap'
